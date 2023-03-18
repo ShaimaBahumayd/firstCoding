3f3de05f4329f20788d2147dc61279f472e62e79
ShaimaBahumayd <128201991+ShaimaBahumayd@users.noreply.github.com>
2023-03-18 07:44:39 +0300

    Update Git1.java

--- src/git/pkg1/Git1.java
+++ src/git/pkg1/Git1.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package git.pkg1;
+import java.util.Scanner;
 
 /**
  *
@@ -15,7 +16,18 @@ public class Git1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       Scanner input =new Scanner (System.in);
+    char a [][]={{'A','B','A','C','C','D'},{'D','B','A','B','C','A'},{'E','D','D','A','C','B'},{'C','B','A','E','D','C'}};
+   
+    char key []={'D','B','D','C','C','D'};
+    
+    for(int i=0;i<a.length;i++){
+        int c =0;
+    for(int j=0 ;j<a[i].length;j++){
+    if(a[i][j]==key[j])
+        c++;
     }
+    System.out.println("Studen "+i+"'s correct count is "+c+" out of "+a[i].length);
+    }    }
     
 }
